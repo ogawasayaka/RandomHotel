e7331d164eff1de16e365d9c702d9bc090edf688
ogawasayaka <sayaka.i333@gmail.com>
2023-12-22 05:27:18 +0900

    add: 利用規約,

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -5,4 +5,8 @@ class HotelsController < ApplicationController
     @random_id = Hotel.pluck(:id).sample
     @hotel = Hotel.find_by(id: @random_id)
   end
+
+  def privacy; end
+
+  def term; end
 end
